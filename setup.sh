8391427e5d9af5771ec25320554335d9eb7ced07
Gokhan Ozdemir <gokhanworkacc@gmail.com>
2024-06-26 18:26:59 +0300

    setup.sh configured again for file-name change to psql-labv1

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 # Clone the repository
 git clone https://github.com/ZeePLA/postgres-lab.git
-cd psql-labv1
+cd postgres-lab
 
 # Build the Docker image
-docker build -t psql-labv1 ./
+docker build -t psql-labv1 .
 
 # Run the Docker container
 docker run -d -p 5432:5432 --name psql-lab1 psql-labv1
